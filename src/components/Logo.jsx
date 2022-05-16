18061ff069f885a490f84deb0608d73ee8baa05a
Owan Hunte <owanh@hotmail.com>
2022-05-16 12:35:39 -0400

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import NextLink from 'next/link';
 
 function Logo() {
 	return (
-		<div style={{ display: "flex", width: "280px", fontSize: "17px" }}>
+		<div style={{ display: "flex", width: "320px", fontSize: "17px", lineHeight: "1.0" }}>
 			<NextLink href="/">
 				<a>Algomint Bridge v2 POC</a>
 			</NextLink>
