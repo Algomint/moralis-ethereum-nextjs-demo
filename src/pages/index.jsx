81e83874e26fa843acb40af5259e5ce17875134d
Owan Hunte <owanh@hotmail.com>
2022-05-16 12:18:00 -0400

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Index() {
-	return <div>Pppf!!</div>;
+	return <div>Algomint Bridge v2 Proof-of-Concept demo. Please choose from one of the above menu options.</div>;
 }
 
 export default Index;
